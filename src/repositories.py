fa8bb881260c87973e7023f47cd9525db0034401
鬼王 <THRAEX@aliyun.com>
2019-11-23 20:36:47 +0800

    save: Repositories class

--- src/repositories.py
+++ src/repositories.py
@@ -3,6 +3,35 @@
 from git import Repo
 import os, shutil
 
+class Repositories(object):
+	def __init__(self, env = 'test'):
+		super(Repositories, self).__init__()
+		self.source = config.SOURCE
+		self.target = config.TARGET.get(env, config.TARGET['test'])
+
+	def run(self):
+		projects = []
+		for project in projects:
+			# uri = '%s.git' % project['path']
+			namespace = project['path']
+			tmppath = 'tmp/repos/%s' % namespace
+			self.clean(tmppath)
+			uri = '%s.git' % namespace
+			self.pull(uri)
+			self.push(uri)
+
+	def pull(self, uri):
+		url = 'http://%s/%s' % (self.source['address'], uri)
+		print('pull url:', url)
+
+	def push(self, uri):
+		url = 'http://%s/%s' % (self.target['address'], uri)
+		print('push url:', url)
+
+	def clean(self, dirpath):
+		if os.path.exists(dirpath):
+			shutil.rmtree(dirpath, onerror = onerror)
+
 project = 'admin-ui'
 dirpath = 'tmp/repos/%s' % project
 
